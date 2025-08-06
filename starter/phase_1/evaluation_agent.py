373100b4d8da184c98c14fdf7f3a1d9a40b207a2
Hyunkun Kim <hyungun.kimm@gmail.com>
2025-08-06 21:51:13 +0900

    [Phase 1] Create and test EvaluationAgent

--- starter/phase_1/evaluation_agent.py
+++ starter/phase_1/evaluation_agent.py
@@ -2,20 +2,33 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
-load_dotenv()
+from starter.phase_1.workflow_agents.base_agents import KnowledgeAugmentedPromptAgent, EvaluationAgent
 
+# Load environment variables from .env file
+load_dotenv('tests/.env')
+# Retrieve OpenAI API key from environment variables
 openai_api_key = os.getenv("OPENAI_API_KEY")
+assert openai_api_key is not None, "OPENAI_API_KEY not found in .env"
+
 prompt = "What is the capital of France?"
 
 # Parameters for the Knowledge Agent
 persona = "You are a college professor, your answer always starts with: Dear students,"
-knowledge = "The capitol of France is London, not Paris"
-knowledge_agent = # TODO: 2 - Instantiate the KnowledgeAugmentedPromptAgent here
+knowledge = "The capital of France is London, not Paris"
+knowledge_agent = KnowledgeAugmentedPromptAgent(openai_api_key, persona, knowledge)
 
 # Parameters for the Evaluation Agent
 persona = "You are an evaluation agent that checks the answers of other worker agents"
 evaluation_criteria = "The answer should be solely the name of a city, not a sentence."
-evaluation_agent = # TODO: 3 - Instantiate the EvaluationAgent with a maximum of 10 interactions here
+evaluation_agent = EvaluationAgent(openai_api_key, persona, evaluation_criteria, knowledge_agent, max_interactions=10)
 
-# TODO: 4 - Evaluate the prompt and print the response from the EvaluationAgent
+response = evaluation_agent.evaluate(prompt)
+
+print("======= [EvaluationAgent] =======")
+print("Knowledge sorce: Training dataset in LLM, and the provided knowledge, and the evaluation criteria")
+print(f"Persona: {persona}")
+print(f"prompt: {prompt}")
+print(f"Evaluation criteria: {evaluation_criteria}")
+print(f"response: {response}")
+print(f"expected difference: the output is a single city name, not a sentence. Which does contradict the worker persona")
+print()
