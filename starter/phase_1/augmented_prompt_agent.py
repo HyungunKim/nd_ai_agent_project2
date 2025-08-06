fb895791b1f2ad31e0f48989095211c6aaed50b7
Hyunkun Kim <hyungun.kimm@gmail.com>
2025-08-06 09:49:24 +0900

    [Phase 1] Implement and test AugmentedPromptAgent

--- starter/phase_1/augmented_prompt_agent.py
+++ starter/phase_1/augmented_prompt_agent.py
@@ -1,23 +1,31 @@
 # TODO: 1 - Import the AugmentedPromptAgent class
 import os
 from dotenv import load_dotenv
+from workflow_agents.base_agents import AugmentedPromptAgent
 
 # Load environment variables from .env file
-load_dotenv()
-
+load_dotenv('tests/.env')
 # Retrieve OpenAI API key from environment variables
 openai_api_key = os.getenv("OPENAI_API_KEY")
-
+assert openai_api_key is not None, "OPENAI_API_KEY not found in .env"
 prompt = "What is the capital of France?"
 persona = "You are a college professor; your answers always start with: 'Dear students,'"
 
 # TODO: 2 - Instantiate an object of AugmentedPromptAgent with the required parameters
-
+agent = AugmentedPromptAgent(openai_api_key, persona)
 # TODO: 3 - Send the 'prompt' to the agent and store the response in a variable named 'augmented_agent_response'
-
+augmented_agent_response = agent.respond(prompt)
 # Print the agent's response
 print(augmented_agent_response)
 
 # TODO: 4 - Add a comment explaining:
 # - What knowledge the agent likely used to answer the prompt.
 # - How the system prompt specifying the persona affected the agent's response.
+
+print("======= [AugmentedPromptAgent] =======")
+print("Knowledge sorce: Training dataset in LLM ")
+print(f"Persona: {persona}")
+print(f"prompt: {prompt}")
+print(f"response: {augmented_agent_response}")
+print(f"expected difference: the output starts with 'Dear students,'")
+print()
